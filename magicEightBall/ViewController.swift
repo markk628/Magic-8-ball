32c2cd59cff18bef179ef02a96f823c60e6eb676
Donghyun <markk628@gmail.com>
2020-02-12 10:31:56 -0800

    first commit

---
+++ magicEightBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  magicEightBall
+//
+//  Created by Mark Kim on 2/11/20.
+//  Copyright © 2020 Makeschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
